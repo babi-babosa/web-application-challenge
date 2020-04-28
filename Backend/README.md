0e3ca011acf6b4a5f554ae11c882c3f6ed5447cf
Bárbara <1160719@isep.ipp.pt>
2020-04-28 19:46:44 +0100

    Init backend application - node + express

---
+++ Backend/README.md
@@ -0,0 +1,12 @@
+# Instalação
+
+## Mongoose (MongoDB ORM)
+npm install mongoose
+
+## Mongo db (Mongo db in cloud - atlas)
+needs access to get mongodb data
+
+
+
+npm init -y :: create node js project
+npm i -D nodemon :: install nodemon to refresh every change we make
---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+// const port = process.env.PORT || "8000";
+const port = "3000";
+
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
