abed3af370e4f3db94288ac170cd7d3f5ae9c154
Bárbara <1160719@isep.ipp.pt>
2020-04-28 19:55:24 +0100

    Add essecial routes for the project

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,34 @@ app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
 });
 
+
+/**
+ * CRUD - For list of countries (list is public, others are protected with basic authentication)
+ */
+// LIST
+app.get("/api/v1/countries", (req, res) => {
+    res.status(200).send("List all countries inside database");
+});
+
+// CREATE
+app.post("/api/v1/countries", (req, res) => {
+    res.status(200).send("Add a new country");
+});
+
+// UPDATE
+app.put("/api/v1/countries", (req, res) => {
+    res.status(200).send("Update a country");
+});
+
+// DELETE
+app.delete("/api/v1/countries", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
+
+
+/**
+ * Listen REST Api 
+ */
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
