721872aef935fedf2ec202c750a043a24524a470
Bárbara <1160719@isep.ipp.pt>
2020-05-03 23:22:04 +0100

    Finish documentation and small updates

--- Backend/helpers/ResponseHandler.js
+++ Backend/helpers/ResponseHandler.js
@@ -1,8 +1,17 @@
 module.exports = class ResponseHandler {
+    /**
+     * ResponseHandler constructor
+     */
     constructor() {
     }
 
-    errorResponseFormatter (message = "Unkown error", errorStatus = 400) {
+    /**
+     * errorResponseFormatter - Function to return a response that will be used to construct server error response
+     * 
+     * @param {string} message - default is an unknown error 
+     * @param {number} errorStatus - default is an internal server error (HTTP 500)
+     */
+    errorResponseFormatter (message = "Unkown error", errorStatus = 500) {
         return {
             errorStatus: errorStatus,
             response: {
@@ -11,6 +20,11 @@ module.exports = class ResponseHandler {
         };
     }
 
+    /**
+     * successResponseFormatter - Function to return a response that will be used to construct server success response in some cases
+     * 
+     * @param {string} message - default is success 
+     */
     successResponseFormatter (message = "Success") {
         return {
             response: {
